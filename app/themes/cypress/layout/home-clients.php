51d30a535036ddb7e4f18ee939ef83fb4e0b8d46
Giammarco Galletti <gallettigr@dotsqr.co>
2015-06-01 18:35:21 +0200

    fixed small bug on core plugin

--- app/themes/cypress/layout/home-clients.php
+++ app/themes/cypress/layout/home-clients.php
@@ -4,13 +4,15 @@
  */
 
 ?>
-<?php $posts = cypress_query( 'clients', array( 'posts_per_page' => -1, 'post_type' => 'clients' ) ); if($posts) : ?>
+<?php $posts = cypress_query( 'clients', array( 'posts_per_page' => -1, 'post_type' => 'clients' ), 24, false, 12 ); if($posts) : ?>
 <section id="clients" class="clients">
   <div class="container">
     <h4>Abbiamo collaborato con</h4>
     <div class="gallery">
       <?php foreach ($posts as $post) : ?>
-        <?php the_post_thumbnail( 'preview', array( 'class' => 'svg', 'id' => $post->post_name ) ); ?>
+        <div class="item col-md-2 col-xs-6">
+          <?php the_post_thumbnail( 'preview', array( 'id' => $post->post_name ) ); ?>
+        </div>
       <?php endforeach; wp_reset_query(); ?>
     </div>
   </div>
