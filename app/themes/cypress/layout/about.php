1603d47af7da997a2e46b18824affcb8c3afe309
Giammarco Galletti <gallettigr@dotsqr.co>
2015-06-16 13:02:12 +0200

    extended theme functions

--- app/themes/cypress/layout/about.php
+++ app/themes/cypress/layout/about.php
@@ -15,30 +15,7 @@ get_header(); ?>
   <?php endwhile; endif; ?>
   </div>
 </section>
-<section class="about profile">
-  <div class="container">
-    <?php $sections = cypress_get_meta( 'about_sections' ); $i = 0; foreach ($sections as $section) : $section = (object) $section; ?>
-    <?php if(++$i%2) : ?>
-    <div id="<?php echo $section->title ?>" class="row odd">
-      <div class="col-md-6">
-        <div class="number"><span><?php echo $i; ?></span><?php echo $section->title; ?></div>
-        <p><?php echo $section->section_body ?></p>
-      </div>
-      <div class="col-md-6">
-        <?php echo wp_get_attachment_image( $section->section_image, 'square', array('class' => 'img-responsive') ); ?>
-      </div>
-    </div>
-    <?php else: ?>
-    <div id="<?php echo $section->title ?>" class="row even">
-      <div class="col-md-6">
-        <?php echo wp_get_attachment_image( $section->section_image, 'square', array('class' => 'img-responsive') ); ?>
-      </div>
-      <div class="col-md-6">
-        <div class="number"><span><?php echo $i; ?></span><?php echo $section->title; ?></div>
-        <p><?php echo $section->section_body ?></p>
-      </div>
-    </div>
-    <?php endif; endforeach; ?>
-  </div>
-</section>
+
+<?php get_template_part('layout/about', 'profile'); ?>
+
 <?php get_footer(); ?>
